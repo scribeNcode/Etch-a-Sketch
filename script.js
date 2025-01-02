93f31fbcb3c332500f3cd10e78e9e81b775b3734
scribeNcode <deyeye99@gmail.com>
2025-01-02 08:01:39 +0100

    add event listeners to div to set hover effect
    add event listeners to add background color to
    divs when mouse is on div and removes the
    background when cursor moves out of the div

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ let numberOfSquareDivs = divsPerSides ** 2;
 
 for(let i = 1; i <= numberOfSquareDivs; i++){
     const eachDivs = document.createElement('div');
-    eachDivs.classList.add("eachDivs")
+    eachDivs.classList.add("eachDivs");
+    // add eventlister to set background of div when mouser hover on it 
+    eachDivs.addEventListener('mouseenter', ()=>{
+        eachDivs.style.backgroundColor = "red"
+    });
+
+        // add eventlister to remove  of div background when mouser leaves the div 
+    eachDivs.addEventListener('mouseleave', ()=>{
+        eachDivs.style.backgroundColor = ""
+    });
     divWrapper.appendChild(eachDivs)
 }
